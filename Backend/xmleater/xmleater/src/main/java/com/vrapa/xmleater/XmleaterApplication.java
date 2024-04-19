96d51a0fc3890bfb67998cd16ad90f16102996b6
Abhinav Verma <abhinav65vrma@gmail.com>
2024-04-19 09:25:12 +0000

    Add files via upload

---
+++ Backend/xmleater/xmleater/src/main/java/com/vrapa/xmleater/XmleaterApplication.java
@@ -0,0 +1,13 @@
+package com.vrapa.xmleater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmleaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmleaterApplication.class, args);
+	}
+
+}
---
+++ Backend/xmleater/xmleater/src/test/java/com/vrapa/xmleater/XmleaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrapa.xmleater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmleaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
