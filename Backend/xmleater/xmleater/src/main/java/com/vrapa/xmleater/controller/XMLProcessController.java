13835212e4b083c26cbd76df3059a5663240aca0
Vinit Oswal <44002802+VineetOS@users.noreply.github.com>
2024-04-19 10:21:37 +0000

    test controller

--- Backend/xmleater/xmleater/src/main/java/com/vrapa/xmleater/controller/XMLProcessController.java
+++ Backend/xmleater/xmleater/src/main/java/com/vrapa/xmleater/controller/XMLProcessController.java
@@ -1,8 +1,17 @@
 package com.vrapa.xmleater.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("xmlEater")
+@CrossOrigin
 public class XMLProcessController {
 
+    @GetMapping("/test")
+    public String getTest(){
+        return "Test Request";
+    }
 }
--- Backend/xmleater/xmleater/src/main/java/com/vrapa/xmleater/service/XMLProcessService.java
+++ Backend/xmleater/xmleater/src/main/java/com/vrapa/xmleater/service/XMLProcessService.java
@@ -1,4 +1,7 @@
 package com.vrapa.xmleater.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class XMLProcessService {
 }
